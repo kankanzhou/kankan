7539cd01bb7f9dba08d917cbbd46632001c2b1c5
kankanzhou <kan.kan.zhou@outlook.com>
2020-10-20 15:24:11 +0000

    done

---
+++ prediction.py
@@ -0,0 +1,37 @@
+
+import torch
+import my_model
+from torchvision import datasets, transforms
+
+# Define a transform to normalize the data
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5,), (0.5,))])
+
+# Download and load the test data
+testset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=False, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
+
+
+model = my_model.Network(784, 10, [512, 256, 128])
+
+checkpoint = torch.load('./checkpoint.pth')
+
+model.load_state_dict(checkpoint)
+
+model.eval()
+
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+img = images[0]
+# Convert 2D image to 1D vector
+img = img.view(1, 784)
+
+# Calculate the class probabilities (softmax) for img
+with torch.no_grad():
+    output = model.forward(img)
+
+ps = torch.exp(output)
+
+print(ps.topk(1, dim=1))
+print(labels[0])
+
